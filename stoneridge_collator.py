61f4ec11b20bcc96dd89f5da98477895c983ff2b
Nick Hurley <hurley@todesschaf.org>
2012-04-17 16:50:00 -0700

    Expand TODO for remindering

--- stoneridge_collator.py
+++ stoneridge_collator.py
@@ -2,4 +2,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-# TODO
+
+# TODO - This will take all the data we've collected from our tests and put
+# it into a nice format that the graph server can handle, and save it off
+# to the json file we will upload
