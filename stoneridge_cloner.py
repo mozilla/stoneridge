159ed7dd9dd5ade6c4310d2aa106b13bf94aa903
Nick Hurley <hurley@todesschaf.org>
2012-04-17 16:50:11 -0700

    Add empty cloner

---
+++ stoneridge_cloner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+# TODO - This will run on the central server, and download releases from ftp.m.o
+# to a local directory for serving up to the clients, which will download the
+# necessary stuff via stoneridge_downloader.py
