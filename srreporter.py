f408f8d2c645d08f1a0abe806b0aac6878c1bcd5
Nick Hurley <hurley@todesschaf.org>
2013-01-10 11:02:07 -0800

    Make reporter use mq

--- srreporter.py
+++ srreporter.py
@@ -4,85 +4,82 @@
 # obtain one at http://mozilla.org/MPL/2.0/.
 
 import argparse
+import base64
 import dzclient
-import glob
 import json
 import logging
 import os
-import shutil
-import time
 
 import stoneridge
 
-class StoneRidgeReporter(object):
-    def __init__(self):
+class StoneRidgeReporter(stoneridge.QueueListener):
+    def setup(self):
         self.rootdir = stoneridge.get_config('server', 'uploads')
-        self.pattern = os.path.join(self.rootdir, '*.json')
         self.host = stoneridge.get_config('report', 'host')
         self.project = stoneridge.get_config('report', 'project')
         self.key = stoneridge.get_config('report', 'key')
         self.secret = stoneridge.get_config('report', 'secret')
         self.archives = stoneridge.get_config('report', 'archives')
 
-        logfile = os.path.join(self.archives, '%s.log' % (int(time.time()),))
-        logging.basicConfig(filename=logfile, level=logging.DEBUG,
-                format=stoneridge.log_fmt)
         logging.debug('root directory: %s' % (self.rootdir,))
-        logging.debug('pattern: %s' % (self.pattern,))
-        logging.debug('host: %s' % (self.host,))
+        logging.debug('report host: %s' % (self.host,))
         logging.debug('project: %s' % (self.project,))
         logging.debug('oauth key: %s' % (self.key,))
         logging.debug('oauth secret: %s' % (self.secret,))
         logging.debug('archives: %s' % (self.archives,))
 
-    def run(self):
-        files = glob.glob(self.pattern)
-        logging.debug('files to upload: %s' % (files,))
-        for fpath in files:
-            logging.debug('reading %s' % (fpath,))
-            with file(fpath, 'rb') as f:
-                try:
-                    dataset = json.load(f)
-                    logging.debug('read data: %s' % (dataset,))
-                except:
-                    # This one is crap, trash it so we never try it again
-                    logging.debug('bad json: %s' % (f.read(),))
-                    logging.debug('deleting bad data')
-                    os.unlink(fpath)
-                    continue
+    def save_data(self, srid, results, metadata_b64):
+        archivedir = os.path.join(self.archives, srid)
+        os.makedirs(archivedir)
 
-            logging.debug('uploading data')
-            request = dzclient.DatazillaRequest('https', self.host,
-                    self.project, self.key, self.secret)
-            response = request.send(dataset)
-            logging.debug('got status code %s' % (response.status,))
-            if response.status != 200:
-                logging.debug('bad status code')
-                continue
+        results_file = os.path.join(archivedir, 'results.json')
+        with file(results_file, 'w') as f:
+            f.write(results)
 
+        metadata = base64.b64decode(metadata_b64)
+        metadata_file = os.path.join(archivedir, 'metadata.zip')
+        with file(metadata_file, 'wb') as f:
+            f.write(metadata)
+
+    def handle(self, srid, results, metadata_b64)):
+        logging.debug('uploading results for %s' % (srid,))
+
+        try:
+            dataset = json.loads(results)
+            logging.debug('read data: %s' % (dataset,))
+        except:
+            # This one is crap, ignore it
+            logging.error('bad json: %s' % (results,))
+            self.save_data(srid, results, metadata_b64)
+            return
+
+        logging.debug('uploading data')
+        request = dzclient.DatazillaRequest('https', self.host,
+                self.project, self.key, self.secret)
+        response = request.send(dataset)
+        logging.debug('got status code %s' % (response.status,))
+        if response.status != 200:
+            logging.error('bad http status %s for %s' % (response.status, srid))
+
+        try:
             result = json.load(response)
-            logging.debug('got result %s' % (result,))
-            if result['status'] != 'well-formed JSON stored':
-                logging.debug('bad status message')
-                continue
+        except:
+            result = ''
+        logging.debug('got result %s' % (result,))
+        if result['status'] != 'well-formed JSON stored':
+            logging.error('bad status for %s: %s' % (srid, result['status']))
 
-            # Keep a copy of the data around so we can do archaeology later
-            fname = os.path.basename(fpath)
-            fdst = os.path.join(self.archives, fname)
-            logging.debug('mv %s => %s' % (fpath, fdst))
-            shutil.copyfile(fpath, fdst)
-
-            # If we get here, everything went ok, so we can delete the file
-            logging.debug('deleting uploaded file')
-            os.unlink(fpath)
+        self.save_data(srid, results, metadata_b64)
 
 @stoneridge.main
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', dest='config', required=True)
+    parser.add_argument('--log', dest='log', required=True)
+    parser.add_argument('--host', dest='host', required=True)
     args = parser.parse_args()
 
     stoneridge._conffile = args.config
 
-    reporter = StoneRidgeReporter()
+    reporter = StoneRidgeReporter(args.host, stoneridge.OUTGOING_QUEUE)
     reporter.run()
