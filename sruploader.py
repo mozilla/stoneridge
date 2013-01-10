4f0a028bf0b26f677d96bf81636720c33f8be0a3
Nick Hurley <hurley@todesschaf.org>
2013-01-10 12:24:12 -0800

    Make uploader send to queue instead of a server

--- sruploader.py
+++ sruploader.py
@@ -3,42 +3,47 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 
-import glob
+import base64
+import logging
 import os
-import requests
 
 import stoneridge
-
-import logging
 
 class StoneRidgeUploader(object):
     """Takes the upload files created by the collator and uploads them to the
     graph server
     """
-    def __init__(self):
-        self.url = stoneridge.get_config('upload', 'url')
-        logging.debug('upload url: %s' % (self.url,))
+    def __init__(self, host):
+        self.queue = stoneridge.QueueWriter(host, stoneridge.OUTGOING_QUEUE)
 
     def run(self):
         logging.debug('uploader running')
-        file_pattern = os.path.join(stoneridge.outdir, 'upload_*.json')
-        upload_files = glob.glob(file_pattern)
-        logging.debug('files to upload: %s' % (upload_files,))
-        if not upload_files:
+
+        if not os.path.exists(stoneridge.results_file):
             # Nothing to do, so forget it!
+            logging.debug('no file to upload')
             return
-        files = {os.path.basename(fname): open(fname, 'rb')
-                 for fname in upload_files}
-        logging.debug('uploading files')
-        requests.post(self.url, files=files)
-        logging.debug('closing file handles')
-        for f in files.values():
-            f.close()
+
+        with file(stoneridge.upload_file) as f:
+            results = f.read()
+
+        if os.path.exists(stoneridge.metadata_file):
+            with file(stoneridge.metadata_file) as f:
+                contents = f.read()
+            metadata = base64.b64encode(contents)
+        else:
+            # Missing metadata, but we can still report results
+            logging.warning('missing metadata, continuing anyway')
+            metadata = base64.b64encode('')
+
+        self.queue.enqueue(srid=stoneridge.srid, results=results,
+                metadata=metadata)
 
 @stoneridge.main
 def main():
     parser = stoneridge.ArgumentParser()
+    parser.add_argument('--host', dest='host', required=True)
     args = parser.parse_args()
 
-    uploader = StoneRidgeUploader()
+    uploader = StoneRidgeUploader(args.host)
     uploader.run()
