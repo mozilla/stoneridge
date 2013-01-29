298c502539bb45410e205bc657327e3c7d9f9906
Nick Hurley <hurley@todesschaf.org>
2013-01-29 14:11:00 -0800

    Fix daemonization for stoneridge programs

--- stoneridge.py
+++ stoneridge.py
@@ -5,11 +5,13 @@
 import argparse
 import ConfigParser
 import copy
-import daemonize
+import fcntl
 import inspect
 import logging
 import os
 import platform
+import resource
+import signal
 import subprocess
 import sys
 import traceback
@@ -286,6 +288,96 @@ class ArgumentParser(argparse.ArgumentParser):
         return args
 
 
+def daemon_sig(pidfile):
+    """Signal handler for daemons created with stoneridge.daemonize.
+    """
+    logging.debug('signal handler: unlinking pidfile')
+    os.unlink(pidfile)
+    logging.debug('signal handler: daemon exiting')
+    sys.exit(0)
+
+
+def daemonize(pidfile, function, **kwargs):
+    """Run a function as a daemon.
+
+    pidfile - Name of file to write PID to
+    function - Function object to call as the daemon
+    kwargs - Arguments to pass to <function>
+    """
+    logging.debug('forking for daemonization')
+    pid = os.fork()
+
+    if pid < 0:
+        # Fork failure
+        logging.error('fork failed (%s)' % (os.strerror(pid,)))
+        sys.exit(1)
+
+    if pid:
+        # Parent
+        sys.exit(0)
+
+    sid = os.setsid()
+    if sid == -1:
+        # Error setting session ID
+        logging.error('error setting sid')
+        sys.exit(1)
+
+    devnull = getattr(os, 'devnull', '/dev/null')
+    logging.debug('devnull = %s' % (devnull,))
+
+    log_fds = set()
+    logger = logging.getLogger()
+    for handler in logger.handlers:
+        if isinstance(handler, logging.FileHandler):
+            log_fds.add(handler.stream.fileno())
+    logging.debug('log fds = %s' % (log_fds,))
+
+    for fd in range(resource.getrlimit(resource.RLIMIT_NOFILE)[0]):
+        if fd in log_fds:
+            logging.debug('not closing fd %s (log)' % (fd,))
+        else:
+            try:
+                os.close(fd)
+                logging.debug('closed fd %s' % (fd,))
+            except OSError:
+                # Didn't have it open, don't care
+                pass
+
+    # Make stdin, stdout & stderr point to /dev/null
+    logging.debug('pointing std{in,out,err} -> devnull')
+    os.open(devnull, os.O_RDWR)
+    os.dup(0)
+    os.dup(0)
+
+    # Set a sane umask
+    logging.debug('setting umask 027')
+    os.umask(027)
+
+    # Change to the usual daemon directory
+    logging.debug('chdir -> /')
+    os.chdir('/')
+
+    with file(pidfile, 'w') as f:
+        logging.debug('locking %s' % (pidfile,))
+        fcntl.lockf(f, fcntl.LOCK_EX|fcntl.LOCK_NB)
+
+        logging.debug('writing pid')
+        f.write('%s' % (os.getpid(),))
+        f.flush()
+
+        logging.debug('setting up sigterm handler')
+        signal.signal(signal.SIGTERM, lambda sig, frame: daemon_sig(pidfile))
+
+        logging.debug('calling daemon function')
+        function(**kwargs)
+
+        # If we get here, we assume the program is exiting cleanly
+        logging.debug('unlinking pidfile')
+        os.unlink(pidfile)
+        logging.debug('daemon exiting')
+        sys.exit(0)
+
+
 class DaemonArgumentParser(ArgumentParser):
     """An argument parser for stone ridge programs that run as daemons.
     """
@@ -318,19 +410,14 @@ class DaemonArgumentParser(ArgumentParser):
 
         return self.args
 
-    def daemon(self):
-        self.daemon_func(**self.daemon_kwargs)
-
     def start_daemon(self, daemon_func, **kwargs):
         if self.args.nodaemon:
+            logging.debug('not running daemonized')
             daemon_func(**kwargs)
             sys.exit(0)
 
-        self.daemon_func = daemon_func
-        self.daemon_kwargs = kwargs
-
-        daemonize.start(self.daemon, self.args.pidfile, self.args._sr_log_,
-                debug=True)
+        logging.debug('starting daemon')
+        daemonize(self.args.pidfile, daemon_func, **kwargs)
 
 
 class TestRunArgumentParser(ArgumentParser):
@@ -389,6 +476,7 @@ class QueueListener(object):
     def run(self):
         """Main event loop for a queue listener.
         """
+        logging.debug('Running queue listener for %s' % (self._queue,))
         if self._queue is None:
             raise Exception('You must set queue for %s' % (type(self),))
 
