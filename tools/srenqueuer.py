e781229453a5d6d654c6ab6acae5ad2866b28f9c
Nick Hurley <hurley@todesschaf.org>
2013-02-19 12:57:25 -0800

    Handle exceptions better in enqueuer
    We don't care too much, so just swallow them. People will complain at me
    if their "pushed" jobs don't get run (eventually).

--- tools/srenqueuer.py
+++ tools/srenqueuer.py
@@ -19,13 +19,26 @@ def main():
     username = stoneridge.get_config('enqueuer', 'username')
     password = stoneridge.get_config('enqueuer', 'password')
 
-    res = requests.get(root + '/list_unhandled', auth=(username, password))
+    try:
+        res = requests.get(root + '/list_unhandled', auth=(username, password))
+    except:
+        # For some reason, we sometimes get a requests failure here, even though
+        # everything seems to be working fine. Ignore that, and try again later.
+        return
+
     queue = json.loads(res.text)
 
     for entry in queue:
+        try:
+            requests.post(root + '/mark_handled', data={'id': entry['pushid']},
+                    auth=(username, password))
+        except:
+            # If we fail to mark this as handled, wait until the next try so we
+            # don't run the same thing more than once. It's not the end of the
+            # world ot have to wait...
+            return
+
         stoneridge.enqueue(nightly=False, ldap=entry['ldap'], sha=entry['sha'],
                 netconfigs=entry['netconfigs'],
                 operating_systems=entry['operating_systems'],
                 srid=entry['srid'])
-        requests.post(root + '/mark_handled', data={'id': entry['pushid']},
-                auth=(username, password))
