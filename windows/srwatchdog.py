359ffffc4e59409acf4bf08bf5521a7fdf99540a
Nick Hurley <hurley@todesschaf.org>
2013-02-19 13:28:07 -0800

    Watchdog process for windows
    In case the worker fails somehow, this will restart it infinitely

---
+++ windows/srwatchdog.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+prog_args = [sys.executable] + sys.argv[1:]
+
+while True:
+    p = subprocess.Popen(prog_args, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    p.wait()
