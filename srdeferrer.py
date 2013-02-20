e87b3f0c66607c0316c9daa1c95ad7ab53c6b160
Nick Hurley <hurley@todesschaf.org>
2013-02-19 16:22:00 -0800

    Fix lint warnings in deferrer

--- srdeferrer.py
+++ srdeferrer.py
@@ -12,7 +12,7 @@ import stoneridge
 
 class StoneRidgeDeferrer(object):
     def __init__(self, srid, nightly, ldap, sha, netconfigs, operating_systems,
-            attempt):
+                 attempt):
         self.srid = srid
         self.nightly = nightly
         self.ldap = ldap
@@ -33,9 +33,9 @@ class StoneRidgeDeferrer(object):
             now = int(time.time())
 
         stoneridge.enqueue(srid=self.srid, nightly=self.nightly, ldap=self.ldap,
-                sha=self.sha, netconfigs=self.netconfigs,
-                operating_systems=self.operating_systems,
-                attempt=self.attempt)
+                           sha=self.sha, netconfigs=self.netconfigs,
+                           operating_systems=self.operating_systems,
+                           attempt=self.attempt)
 
 
 def daemon(deferrer, args):
@@ -51,19 +51,20 @@ def main():
     parser.add_argument('--srid', dest='srid', required=True)
     parser.add_argument('--attempt', dest='attempt', type=int, required=True)
     parser.add_argument('--nightly', dest='nightly', action='store_true',
-            default=False)
+                        default=False)
     parser.add_argument('--ldap', dest='ldap', default='')
     parser.add_argument('--sha', dest='sha', default='')
     for nc in stoneridge.NETCONFIGS:
         parser.add_argument('--%s' % (nc,), dest='netconfigs',
-                action='append_const', const=nc, default=[])
+                            action='append_const', const=nc, default=[])
     for ops in stoneridge.OPERATING_SYSTEMS:
         parser.add_argument('--%s' % (ops,), dest='operating_systems',
-                action='append_const', const=ops, default=[])
+                            action='append_const', const=ops, default=[])
 
     args = parser.parse_args()
 
     deferrer = StoneRidgeDeferrer(args.srid, args.nightly, args.ldap, args.sha,
-            args.netconfigs, args.operating_systems, args.attempt)
+                                  args.netconfigs, args.operating_systems,
+                                  args.attempt)
 
     parser.start_daemon(daemon, deferrer=deferrer, args=args)
