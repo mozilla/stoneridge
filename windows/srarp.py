e9bc96b15e8317b1fea2991e4ffd18caf91da1f7
Nick Hurley <hurley@todesschaf.org>
2013-02-22 10:12:57 -0800

    Program to set static ARP entries on windows
    This will hopefully negate some of the spikyness in the windows tests

---
+++ windows/srarp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import subprocess
+
+ip_mappings = {
+    '172.17.0.1': '2C:76:8A:D0:CE:89',  # Broadband
+    '172.18.0.1': '2C:76:8A:D0:CA:D9',  # UMTS
+    '172.19.0.1': '2C:76:8A:D0:CE:B9',  # GSM
+}
+
+for ip, mac in ip_mappings.items():
+    p = subprocess.Popen(['arp', '-s', ip, mac], stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    p.wait()
