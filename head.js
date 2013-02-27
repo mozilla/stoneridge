066250236cba71048751f9041d117c2a72b71fab
Nick Hurley <hurley@todesschaf.org>
2013-02-27 09:47:28 -0800

    Start with some jshint-inspired cleanups to head.js

--- head.js
+++ head.js
@@ -7,6 +7,11 @@
  * test suite. This must be run under xpcshell running in JS v1.8 mode.
  */
 
+/*jshint curly:true, indent:4, latedef:true, undef:true, unused:true,
+  trailing:true, es5:true, esnext:true*/
+/*global Components:true, run_test:true, _SR_OUT_SUBDIR:true,
+   _SR_OUT_FILE:true*/
+
 var STONERIDGE_FINISHED = null;
 var STONERIDGE_RESULTS = null;
 
@@ -18,16 +23,16 @@ var Cr = Components.results;
  * Store some results for writing once we're all done
  */
 function do_write_result(key, start, stop) {
-  var startms = start.valueOf();
-  var stopms = stop.valueOf();
+    var startms = start.valueOf();
+    var stopms = stop.valueOf();
 
-  var val = {'start':startms, 'stop':stopms, 'total':stopms - startms};
+    var val = {'start': startms, 'stop': stopms, 'total': stopms - startms};
 
-  if (STONERIDGE_RESULTS.hasOwnProperty(key)) {
-    STONERIDGE_RESULTS[key].push(val);
-  } else {
-    STONERIDGE_RESULTS[key] = [val];
-  }
+    if (STONERIDGE_RESULTS.hasOwnProperty(key)) {
+        STONERIDGE_RESULTS[key].push(val);
+    } else {
+        STONERIDGE_RESULTS[key] = [val];
+    }
 }
 
 /*
@@ -36,7 +41,7 @@ function do_write_result(key, start, stop) {
  * with.
  */
 function do_test_finish() {
-  STONERIDGE_FINISHED = true;
+    STONERIDGE_FINISHED = true;
 }
 
 /*
@@ -46,44 +51,46 @@ function do_test_finish() {
 function do_test_pending() {}
 
 function _do_save_results() {
-  // Create a file pointing to our output directory
-  var ofile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
-  ofile.initWithPath(_SR_OUT_SUBDIR);
+    // Create a file pointing to our output directory
+    var ofile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+    ofile.initWithPath(_SR_OUT_SUBDIR);
 
-  // And use the file determined by our caller
-  ofile.append(_SR_OUT_FILE);
+    // And use the file determined by our caller
+    ofile.append(_SR_OUT_FILE);
 
-  // Now get an output stream for our file
-  var ostream = Cc["@mozilla.org/network/file-output-stream;1"].
-                createInstance(Ci.nsIFileOutputStream);
-  ostream.init(ofile, -1, -1, 0);
+    // Now get an output stream for our file
+    var ostream = Cc["@mozilla.org/network/file-output-stream;1"].
+                  createInstance(Ci.nsIFileOutputStream);
+    ostream.init(ofile, -1, -1, 0);
 
-  var jstring = JSON.stringify(STONERIDGE_RESULTS);
-  ostream.write(jstring, jstring.length);
-  ostream.close();
+    var jstring = JSON.stringify(STONERIDGE_RESULTS);
+    ostream.write(jstring, jstring.length);
+    ostream.close();
 }
 
 function make_channel(url) {
-  var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-  return ios.newChannel(url, "", null);
+    var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+    return ios.newChannel(url, "", null);
 }
 
 /*
  * The main entry point for all stone ridge tests
  */
 function do_stoneridge() {
-  STONERIDGE_FINISHED = false;
-  STONERIDGE_RESULTS = {};
+    STONERIDGE_FINISHED = false;
+    STONERIDGE_RESULTS = {};
 
-  run_test();
+    run_test();
 
-  // Pump the event loop until we're told to stop
-  var thread = Cc["@mozilla.org/thread-manager;1"].
-               getService().currentThread;
-  while (!STONERIDGE_FINISHED)
-    thread.processNextEvent(true);
-  while (thread.hasPendingEvents())
-    thread.processNextEvent(true);
+    // Pump the event loop until we're told to stop
+    var thread = Cc["@mozilla.org/thread-manager;1"].
+                 getService().currentThread;
+    while (!STONERIDGE_FINISHED) {
+        thread.processNextEvent(true);
+    }
+    while (thread.hasPendingEvents()) {
+        thread.processNextEvent(true);
+    }
 
-  _do_save_results();
+    _do_save_results();
 }
