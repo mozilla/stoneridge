139b996456224d3dee75512482a182fb7711d52d
Nick Hurley <hurley@todesschaf.org>
2013-03-06 10:36:22 -0800

    Set up a dns check for both changes
    This way I can get email no matter what fails

--- srworker.py
+++ srworker.py
@@ -192,15 +192,17 @@ class StoneRidgeWorker(stoneridge.QueueListener):
 
         self.run_process('dnsupdater')
 
-        self.run_process('dnscheck')
-
         self.need_dns_reset = True
+
+        self.run_process('dnscheck')
 
         self.run_process('runner')
 
         self.need_dns_reset = False
 
         self.run_process('dnsupdater', '--restore')
+
+        self.run_process('dnscheck', '--public')
 
         self.run_process('collator')
 
