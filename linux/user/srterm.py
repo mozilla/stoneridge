ecedbbed1f86b19a016ef2f86a14ad655152cfb6
Nick Hurley <hurley@todesschaf.org>
2013-03-13 12:39:41 -0700

    Add wrapper process for worker, beyond the auto-start
    This makes debugging easier on the test setup in VMs

--- linux/user/srterm.py
+++ linux/user/srterm.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
 import subprocess
-import sys
 
-SRHOME = '/home/hurley/srhome'
-SRPYTHON = '%s/stoneridge' % (SRHOME,)
-SRRUN = '%s/srrun.py' % (SRPYTHON,)
-SRWORKER = '%s/srworker.py' % (SRPYTHON,)
-SRINI = '%s/stoneridge.ini' % (SRHOME,)
-LOG = '%s/srworker.log' % (SRHOME,)
+SRWRAPPER = '/home/hurley/srhome/stoneridge/linux/user/srwrapper.py'
 
-cli = [sys.executable, SRRUN, SRWORKER, '--config', SRINI, '--log', LOG]
 p = subprocess.Popen(['/usr/bin/gnome-terminal', '-t', 'Stone Ridge',
-                      '-e', ' '.join(cli)])
+                      '-e', SRWRAPPER])
 p.wait()
