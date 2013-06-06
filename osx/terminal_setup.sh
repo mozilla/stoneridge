1ea3e576ce58e26afff257d56c4ac8f743c8510a
Nick Hurley <hurley@todesschaf.org>
2013-06-05 20:38:58 -0700

    Remove some unused code from the osx terminal setup script

--- osx/terminal_setup.sh
+++ osx/terminal_setup.sh
@@ -3,8 +3,3 @@
 defaults write com.apple.Terminal 'Window Settings' -dict-add 'Stone Ridge' '{ CommandString = "/usr/local/bin/python /Users/stoneridge/stoneridge/srworker.py --config /Users/stoneridge/stoneridge.ini --log /Users/stoneridge/logs/srworker.log"; ProfileCurrentVersion = "2.02"; RunCommandAsShell = 0; name = "Stone Ridge"; type = "Window Settings"; }'
 defaults write com.apple.Terminal 'Default Window Settings' 'Stone Ridge'
 defaults write com.apple.Terminal 'Startup Window Settings' 'Stone Ridge'
-osascript -e 'tell app "System Events"
-make login item at end with properties {path:"/Applications/Utilities/Terminal.app", hidden:false}
-end tell'
-echo "Setting auto login user (needs root privs)"
-sudo defaults write /Library/Preferences/com.apple.loginwindow.plist autoLoginUser stoneridge
